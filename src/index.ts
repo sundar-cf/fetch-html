779a6daa358d1281b342471457cfcc13626983b3
sundar <sundar@cloudflare.com>
2026-03-13 10:11:12 +0800

    changed remote server 1

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ export default {
 		/**
 		 * Replace `remote` with the host you wish to send requests to
 		 */
-		const remote = "https://pinkbunny.shop";
+		const remote = "https://natarajarts.art";
 
 		return await fetch(remote, request);
 	},
